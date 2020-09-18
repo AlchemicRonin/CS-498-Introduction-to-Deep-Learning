29989c40533edef336b166276ceaeaa33f1a7ad3
AlchemicRonin <1216532005@qq.com>
2020-09-19 00:04:12 +0800

    finished Perceptron

--- assignment1/models/Perceptron.py
+++ assignment1/models/Perceptron.py
@@ -4,15 +4,16 @@ import numpy as np
 
 
 class Perceptron:
-    def __init__(self, n_class: int, lr: float, epochs: int):
+    def __init__(self, n_class: int, lr: float, epochs: int, dim: int):
         """Initialize a new classifier.
 
         Parameters:
             n_class: the number of classes
             lr: the learning rate
             epochs: the number of epochs to train for
+            dim: the dimension of an input sample
         """
-        self.w = None  # TODO: change this
+        self.w = np.random.random((n_class, dim + 1))
         self.lr = lr
         self.epochs = epochs
         self.n_class = n_class
@@ -27,8 +28,15 @@ class Perceptron:
                 N examples with D dimensions
             y_train: a numpy array of shape (N,) containing training labels
         """
-        # TODO: implement me
-        pass
+        for epoch in range(self.epochs):
+            for i in range(len(X_train)):
+                train = np.append(X_train[i], 1).T
+                label_ = y_train[i]
+                for class_ in range(self.n_class):
+                    if self.w[class_] @ train > self.w[label_] @ train:
+                        self.w[label_] = self.w[label_] + self.lr * train
+                        self.w[class_] = self.w[class_] - self.lr * train
+        return self.w
 
     def predict(self, X_test: np.ndarray) -> np.ndarray:
         """Use the trained weights to predict labels for test data points.
@@ -42,5 +50,10 @@ class Perceptron:
                 length N, where each element is an integer giving the predicted
                 class.
         """
-        # TODO: implement me
-        return
+        pre_label = np.zeros(X_test.shape[0])
+        for i in range(len(X_test)):
+            test = np.append(X_test[i], 1).T
+            score = self.w @ test
+            pre_label[i] = np.argmax(score)
+        print(pre_label)
+        return pre_label
